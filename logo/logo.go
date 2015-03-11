3e08f9090b89373273609b123c0baaeb4c73ad20
Christine Dodrill <xena@yolo-swag.com>
2015-03-10 22:01:41 -0700

    add draft of logo command

---
+++ logo/logo.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/mgutz/ansi"
+)
+
+func main() {
+	color := ansi.ColorCode("white+b:green")
+
+	fmt.Print(color)
+	fmt.Printf(logo, 0, 0, 0, "Fast", getUptime(), getUsername(), getHostname())
+	fmt.Println(ansi.ColorCode("reset"))
+}
+
+func getUsername() string {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	return u.Username
+}
+
+func getHostname() string {
+	name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+	return name
+}
---
+++ logo/logo_openbsd.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"time"
+
+	"github.com/mgutz/ansi"
+)
+
+var (
+	color = ansi.ColorCode("white+b:green")
+	reset = ansi.ColorCode("reset")
+)
+
+var logo string = `   ____                                            
+  /____\                                           ` + reset + " Ram:      %dM/%dM" + color + `
+  |\   |     __________.__  __         .__         ` + reset + " Pacakges: %d" + color + `
+  | \  |     \______   \__|/  |________|__| ____   ` + reset + " CPU:      %s" + color + `
+  |  \ |      |    |  _/  \   __\_  __ \  |/ ___\  ` + reset + " Uptime:   %s" + color + `
+  |___\|      |    |   \  ||  |  |  | \/  / /_/  > ` + reset + " User:     %s" + color + `
+  |   /|      |______  /__||__|  |__|  |__\___  /  ` + reset + " Hostname: %s" + color + `
+  |  / |             \/                  /_____/   
+  | /  |                                           
+  |/ __|        Version 1.0                        
+  |    |___________________________________________`
+
+func getUptime() string {
+	out, err := exec.Command("sysctl", "-n", "kern.boottime").Output()
+	if err != nil {
+		panic(err)
+	}
+
+	boottimeint, err := strconv.ParseInt(string(out[:len(out)-1]), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	boottime := time.Unix(boottimeint, 0)
+	dur := time.Since(boottime)
+
+	days := int(dur.Hours() / 24)
+	hours := int(dur.Hours()) % 24
+	minutes := int(dur.Minutes()) % 60
+
+	return fmt.Sprintf("%d days, %d hours, %d minutes", days, hours, minutes)
+}
