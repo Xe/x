7ee8c9b40b183cf5d6a8c8aa881045cad21204d1
Christine Dodrill <xena@yolo-swag.com>
2015-03-27 08:15:46 -0700

    Add simple tool to see github status

---
+++ ghstat/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+)
+
+var (
+	messageFlag = flag.Bool("message", false, "show last message?")
+
+	// TODO: implement
+	//shellFlag = flag.Bool("s", false, "show as shell prompt artifact")
+)
+
+func main() {
+	flag.Parse()
+
+	if *messageFlag {
+		m, err := getMessage()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Last message:\n")
+		fmt.Printf("Status:  %s\n", m.Status)
+		fmt.Printf("Message: %s\n", m.Body)
+
+		t, err := time.Parse(time.RFC3339, m.CreatedOn)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Time:    %s\n", t.Format(time.ANSIC))
+	} else {
+		s, err := getStatus()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		t, err := time.Parse(time.RFC3339, s.LastUpdated)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Status: %s (%s)\n", s.Status, t.Format(time.ANSIC))
+	}
+}
