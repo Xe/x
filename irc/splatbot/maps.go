32759080bcdd98454933658a2036619a96e7a073
Christine Dodrill <xena@yolo-swag.com>
2015-08-20 19:10:12 -0700

    Add flouder heights

--- irc/splatbot/maps.go
+++ irc/splatbot/maps.go
@@ -17,6 +17,7 @@ func init() {
 		"1ac0981d03c18576d9517f40461b66a472168a8f14f6a8714142af9805df7b8c": "Bluefin Depot",
 		"c52a7ab7202a576ee18d94be687d97190e90fdcc25fc4b1591c1a8e0c1c299a5": "Kelp Dome",
 		"6a6c3a958712adedcceb34f719e220ab0d840d8753e5f51b089d363bd1763c91": "Moray Towers",
+		"a54716422edf71ac0b3d20fbb4ba5970a7a78ba304fcf935aaf69254d61ca709": "Flounder Heights",
 	}
 }
 
