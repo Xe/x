359b838e78c6f85f54ca105feada0da99019f2e9
Christine Dodrill <xena@yolo-swag.com>
2015-09-01 12:48:28 -0700

    have directory setting, some semblance of logging

--- quickserv/main.go
+++ quickserv/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"flag"
+	"log"
 	"net/http"
 )
 
 var (
 	port = flag.String("port", "3000", "port to use")
+	dir  = flag.String("dir", ".", "directory to serve")
 )
 
 func main() {
 	flag.Parse()
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	log.Printf("Serving %s on port %s", *dir, *port)
 	http.ListenAndServe(":"+*port, nil)
 }
