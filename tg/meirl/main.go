091aedde46be11d03f0b558c28862e3f6a7391ef
Christine Dodrill <me@christine.website>
2017-05-06 11:33:24 -0700

    relay reddit posts to telegram

--- tg/meirl/main.go
+++ tg/meirl/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"runtime"
+	"time"
 
 	"github.com/Xe/ln"
 	"github.com/caarlos0/env"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/turnage/graw"
+	"github.com/turnage/graw/reddit"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
-type config struct {
-	RedditUsername string `env:"REDDIT_USERNAME,required"`
-	RedditPassword string `env:"REDDIT_PASSWORD.required"`
+const appid = "github.com/Xe/x/tg/meirl"
+const version = "0.1-dev"
 
-	TelegramToken string `env:"TELEGRAM_TOKEN,required"`
-	TelegramAdmin string `env:"TELEGRAM_ADMIN,required"`
+type config struct {
+	RedditBotAdmin string   `env:"REDDIT_ADMIN_USERNAME,required"`
+	Subreddits     []string `env:"SUBREDDITS,required"`
+
+	TelegramToken     string `env:"TELEGRAM_TOKEN,required"`
+	TelegramAdmin     string `env:"TELEGRAM_ADMIN,required"`
+	TelegramChannelID int64  `env:"TELEGRAM_CHANNEL_ID,required"`
 }
 
 func main() {
@@ -24,30 +34,58 @@ func main() {
 		ln.Fatal(ln.F{"err": err, "action": "env.Parse"})
 	}
 
-	bot, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
+	tg, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
 	if err != nil {
 		ln.Fatal(ln.F{"err": err, "action": "tgbotapi.NewBotAPI"})
 	}
 
-	bot.Debug = true
+	ln.Log(ln.F{"action": "telegram_active", "username": tg.Self.UserName})
 
-	ln.Log(ln.F{"action": "telegram_active", "username": bot.Self.UserName})
+	userAgent := fmt.Sprintf(
+		"%s on %s %s:%s:%s (by /u/%s)",
+		runtime.Version(), runtime.GOOS, runtime.GOARCH,
+		appid, version, cfg.RedditBotAdmin,
+	)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+	rd, err := reddit.NewScript(userAgent, 5*time.Second)
+	if err != nil {
+		ln.Fatal(ln.F{"err": err, "user_agent": userAgent})
 	}
+	_ = rd
+
+	ln.Log(ln.F{"action": "reddit_connected", "user_agent": userAgent})
+
+	a := &announcer{
+		cfg: &cfg,
+		tg:  tg,
+	}
+
+	stop, wait, err := graw.Scan(a, rd, graw.Config{Subreddits: cfg.Subreddits, Logger: log.New(os.Stderr, "", log.LstdFlags)})
+	if err != nil {
+		ln.Fatal(ln.F{"err": err, "action": "graw.Scan", "subreddits": cfg.Subreddits})
+	}
+	defer stop()
+
+	// This time, let's block so the bot will announce (ideally) forever.
+	if err := wait(); err != nil {
+		ln.Fatal(ln.F{"err": err, "action": "reddit_wait"})
+	}
+}
+
+type announcer struct {
+	cfg *config
+	tg  *tgbotapi.BotAPI
+}
+
+func (a *announcer) Post(post *reddit.Post) error {
+	msg := tgbotapi.NewMessage(a.cfg.TelegramChannelID, fmt.Sprintf("me irl\n%s\n(https://reddit.com%s by /u/%s)", post.URL, post.Permalink, post.Author))
+	_, err := a.tg.Send(msg)
+	if err != nil {
+		ln.Error(err, ln.F{"err": err, "action": "telegram_post"})
+		return err
+	}
+
+	ln.Log(ln.F{"action": "new_post", "url": post.URL, "permalink": post.Permalink, "redditor": post.Author})
+
+	return nil
 }
