d946c3ae10801016cdb53096db2fcd0db20ce31c
Christine Dodrill <me@christine.website>
2017-08-20 17:04:28 -0700

    oops

--- tg/covfefe/commands/spla2n.go
+++ tg/covfefe/commands/spla2n.go
@@ -50,7 +50,7 @@ func (cmd *splattusCommand) Execute(message tgbotapi.Message) error {
 
 	json.Unmarshal(body, st)
 
-	modeInfo := []string{st.Modes.Regular[0].String(), st.Modes.Gachi[0].String(), st.Modes.League[0].String()}
+	modeInfo := []string{st.Modes.Regular[1].String(), st.Modes.Gachi[1].String(), st.Modes.League[1].String()}
 	text := strings.Join(modeInfo, "\n")
 
 	msg := tgbotapi.NewMessage(message.Chat.ID, text)
@@ -72,7 +72,7 @@ func (sm splatoonMode) String() string {
 	now := time.Now()
 	diff := end.Sub(now)
 
-	return fmt.Sprintf("**%s**:\n**Rotation ends at %s** (in %s)\n**Maps**: %s\nRule: %s\n", sm.Mode, end.Format(time.RFC3339), diff, maps, sm.Rule)
+	return fmt.Sprintf("%s:\nRotation ends at %s (in %s)\nMaps: %s\nRule: %s\n", sm.Mode, end.Format(time.RFC3339), diff, maps, sm.Rule)
 }
 
 type splatoonGameMode struct {
