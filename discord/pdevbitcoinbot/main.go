efc46c8733f2e166a35db7f83d1ba08f3adbbdc4
Christine Dodrill <me@christine.website>
2018-02-06 16:50:44 -0800

    pdevbitcoinbot

--- discord/pdevbitcoinbot/main.go
+++ discord/pdevbitcoinbot/main.go
@@ -4,10 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/caarlos0/env"
+	"github.com/codahale/hdrhistogram"
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -24,20 +28,36 @@ type BitstampReply struct {
 }
 
 type dWebhook struct {
-	Content  string `json:"content"`
-	Username string `json:"username"`
+	Content  string   `json:"content,omitifempty"`
+	Username string   `json:"username"`
+	Embeds   []embeds `json:"embeds,omitifempty"`
+}
+
+type embedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline"`
+}
+
+type embedFooter struct {
+	Text string `json:"text"`
+}
+
+type embeds struct {
+	Fields []embedField `json:"fields"`
+	Footer embedFooter  `json:"footer"`
+}
+
+type data struct {
+	Snapshot  *hdrhistogram.Snapshot
+	LastValue *string
 }
 
 var cfg = struct {
 	WebhookURL string `env:"WEBHOOK_URL,required"`
 }{}
 
-func main() {
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func getBitstamp() (*BitstampReply, error) {
 	resp, err := http.Get("https://www.bitstamp.net/api/ticker/")
 	if err != nil {
 		log.Fatal(err)
@@ -50,22 +70,112 @@ func main() {
 		log.Fatal(err)
 	}
 
-	dw := dWebhook{
-		Content:  fmt.Sprintf("Bitcoin value: $%v", bsr.Ask),
-		Username: "Buttcoin",
-	}
+	return &bsr, nil
+}
 
+func sendWebhook(whurl string, dw dWebhook) error {
 	data, err := json.Marshal(&dw)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	resp, err = http.Post(cfg.WebhookURL, "application/json", bytes.NewBuffer(data))
+	resp, err := http.Post(whurl, "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if resp.StatusCode/100 != 2 {
-		log.Fatal(resp.Status)
+		io.Copy(os.Stderr, resp.Body)
+		resp.Body.Close()
+		return fmt.Errorf("status code was %v", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func main() {
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fin, err := os.Open("./data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var d data
+	err = json.NewDecoder(fin).Decode(&d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fin.Close()
+
+	var h *hdrhistogram.Histogram
+	if d.Snapshot == nil {
+		h = hdrhistogram.New(-400, 3000000000, 5)
+	} else {
+		h = hdrhistogram.Import(d.Snapshot)
+	}
+
+	bsr, err := getBitstamp()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bcf, err := strconv.ParseFloat(bsr.Ask, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h.RecordValue(int64(bcf))
+
+	var lv string = "_shrug_"
+
+	if d.LastValue != nil {
+		lv = *d.LastValue
+	}
+
+	dw := dWebhook{
+		Content:  "BITCOIN PRICE TIEM",
+		Username: "Buttcoin",
+		Embeds: []embeds{embeds{
+			Footer: embedFooter{Text: "powered by pdevbitcoinbot, made by Cadey~#1337"},
+			Fields: []embedField{
+				{
+					Name:   "Now",
+					Value:  bsr.Ask,
+					Inline: true,
+				},
+				{
+					Name:   "Last",
+					Value:  lv,
+					Inline: true,
+				},
+				{
+					Name:   "P95",
+					Value:  fmt.Sprint(h.ValueAtQuantile(95)),
+					Inline: true,
+				},
+			},
+		}},
+	}
+
+	err = sendWebhook(cfg.WebhookURL, dw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d.LastValue = &bsr.Ask
+	d.Snapshot = h.Export()
+
+	fout, err := os.Create("./data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.NewEncoder(fout).Encode(&d)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
