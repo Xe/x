a1ad328ec646e56b69b50a322be3de4f182c5f02
Christine Dodrill <me@christine.website>
2018-10-27 08:47:38 -0700

    start tokipona.yaml

---
+++ tokipona/README.md
@@ -0,0 +1,4 @@
+# tokipona
+
+An attempt to put toki pona sentences into a grammatical structure close to how I
+conceptualize the langauge internally.
---
+++ tokipona/doc.go
@@ -0,0 +1,6 @@
+// Package tokipona is a simple package to walk tokiponatokens sentences and
+// decompose them into their component subject, verb and object.
+//
+// Sorry, most of the type and member names are going to be in Toki Pona so I
+// don't have to code switch between English and Toki Pona so much.
+package tokipona
