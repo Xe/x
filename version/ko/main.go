93c1f99c4e29800126a08569e12d804e0de256cd
Christine Dodrill <me@christine.website>
2018-12-08 10:03:49 -0800

    version/ko: be smarter

--- version/ko/main.go
+++ version/ko/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"log"
 	"os"
+	"runtime"
 
 	"github.com/Xe/x/version"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	version.Run(os.Getenv("GO_VERSION"))
+	ver := os.Getenv("GO_VERSION")
+
+	if ver == "" {
+		log.Printf("ko: No GO_VERSION specified (wanted GO_VERSION=%[1]s) specified, assuming %[1]s", runtime.Version())
+	}
+
+	ver = "go" + ver
+
+	version.Run(ver)
 }
