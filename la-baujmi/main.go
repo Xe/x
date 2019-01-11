f8ff657998a46349e9dee2c5c8db9f18e170961e
Christine Dodrill <me@christine.website>
2019-01-10 21:50:46 -0800

    less

--- la-baujmi/main.go
+++ la-baujmi/main.go
@@ -11,29 +11,6 @@ import (
 )
 
 func main() {
-	/*
-			m := golog.NewMachine().Consult(`
-		toki(jan_Kesi).
-		toki(jan_Pola).
-		toki(jan_Kesi, jan_Pola).
-		toki(jan_Kesi, toki_pona).
-		command(ilo_Kesi, toki(ziho, jan_Kesi)).
-		`)
-			if m.CanProve(`toki(jan_Kesi).`) {
-				log.Printf("toki(jan_Kesi). -> jan Kesi li toki.")
-			}
-
-			solutions := m.ProveAll(`toki(jan_Kesi, X).`)
-			for _, solution := range solutions {
-				log.Printf("jan_Kesi li toki e %s", solution.ByName_("X").String())
-			}
-
-			solutions = m.ProveAll(`command(X, toki(ziho, jan_Kesi)).`)
-			for _, solution := range solutions {
-				log.Printf("%s o, toki e jan_Kesi", solution.ByName_("X").String())
-			}
-	*/
-
 	var m golog.Machine
 	m = golog.NewMachine()
 
