10a92ff91a17731b2cef6e9a5aa3de8fe455d721
Christine Dodrill <me@christine.website>
2019-01-12 17:45:55 -0800

    idp: make middleware better, usage directions

---
+++ idp/idpmiddleware/_example/test.go
@@ -0,0 +1,29 @@
+//+build ignore
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/Xe/x/idp/idpmiddleware"
+	"github.com/Xe/x/internal"
+	"within.website/ln/ex"
+)
+
+var (
+	port    = flag.String("port", "6060", "TCP port")
+	server  = flag.String("server", "https://idp.christine.website", "idp server")
+	me      = flag.String("me", "https://christine.website/", "self-identity")
+	selfURL = flag.String("self-url", "http://127.0.0.1:6060/", "redirect URL for the self.")
+)
+
+func main() {
+	internal.HandleStartup()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "success!", http.StatusOK)
+	})
+
+	log.Fatal(http.ListenAndServe(":"+*port, ex.HTTPLog(idpmiddleware.Protect(*server, *me, *selfURL)(http.DefaultServeMux))))
+}
--- idp/idpmiddleware/middleware.go
+++ idp/idpmiddleware/middleware.go
@@ -6,7 +6,10 @@ import (
 	"sync"
 	"time"
 
+	"github.com/kr/pretty"
 	"github.com/pborman/uuid"
+	"within.website/ln"
+	"within.website/ln/opname"
 )
 
 // Protect protects a given URL behind your given idp(1) server.
@@ -16,24 +19,32 @@ func Protect(idpServer, me, selfURL string) func(next http.Handler) http.Handler
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx = opname.With(ctx, "idpmiddleware.Protect.Handler")
 			if r.URL.Path == "/auth/challenge" {
 				v := r.URL.Query()
+				ctx = ln.WithF(ctx, ln.F{"as": me, "state": v.Get("state"), "code": v.Get("code")})
+				ln.Log(ctx, ln.Info("login"))
 				lock.Lock()
-				defer lock.Unlock()
-				if cd := v.Get("code"); codes[cd] == cd {
+				pretty.Println(codes)
+				if cd := v.Get("state"); codes[cd] == cd {
+					ln.Log(ctx, ln.Info("setting cookie"))
 					http.SetCookie(w, &http.Cookie{
-						Name:     "idp",
+						Name:     "auth",
 						Value:    me,
 						HttpOnly: true,
 						Expires:  time.Now().Add(900 * time.Hour),
+						Path:     "/",
+						SameSite: http.SameSiteLaxMode,
 					})
 
-					http.Redirect(w, r, selfURL, http.StatusTemporaryRedirect)
-					return
+					http.Error(w, "success", http.StatusOK)
 				}
+				lock.Unlock()
+				return
 			}
 
-			cookie, err := r.Cookie("idp")
+			cookie, err := r.Cookie("auth")
 			if err != nil {
 				u, err := url.Parse(idpServer)
 				if err != nil {
@@ -50,9 +61,10 @@ func Protect(idpServer, me, selfURL string) func(next http.Handler) http.Handler
 				v := url.Values{}
 				v.Set("me", me)
 				v.Set("client_id", selfURL)
-				v.Set("redirect_uri", selfURL+"/auth/challenge")
+				v.Set("redirect_uri", selfURL+"auth/challenge")
 				v.Set("state", code)
 				v.Set("response_type", "id")
+				u.RawQuery = v.Encode()
 
 				http.Redirect(w, r, u.String(), http.StatusTemporaryRedirect)
 				return
