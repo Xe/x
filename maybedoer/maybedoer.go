6aa771c3a3cdd99a1b82cf48b1d37edc8ded0e06
Christine Dodrill <me@christine.website>
2019-04-05 16:28:20 +0000

    maybedoer: new package

---
+++ maybedoer/maybedoer.go
@@ -0,0 +1,24 @@
+package maybedoer
+
+// Impl sequences a set of actions to be performed via calls to
+// `Maybe` such that any previous error prevents new actions from being
+// performed.
+//
+// This is, conceptually, just a go-ification of the Maybe monad.
+type Impl struct {
+	err error
+}
+
+// Maybe performs `f` if no previous call to a Maybe'd action resulted
+// in an error
+func (c *Impl) Maybe(f func() error) {
+	if c.err == nil {
+		c.err = f()
+	}
+}
+
+// Error returns the first error encountered in the Error chain.
+func (c *Impl) Error() error {
+	return c.err
+}
+
