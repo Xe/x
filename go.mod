a33ba59767a136a2d5ddd70eec4de5d4185566eb
Christine Dodrill <me@christine.website>
2019-11-28 17:34:51 +0000

    detri

--- go.mod
+++ go.mod
@@ -79,3 +79,5 @@ require (
 )
 
 replace github.com/go-interpreter/wagon v0.0.0 => github.com/perlin-network/wagon v0.3.1-0.20180825141017-f8cb99b55a39
+
+go 1.13
--- jbo/detri.go
+++ jbo/detri.go
@@ -3,8 +3,10 @@ package jbo
 import "time"
 
 const (
-	detriFormat = `de'i li 2006 pi'e 01 pi'e 02`
-	detriTemciFormat = `de'i li 2006 pi'e 01 pi'e 02 ti'u 15 pi'e 04 pi'e 05`
+	detriFormat           = `de'i li 2006 pi'e 01 pi'e 02`
+	detriTemciFormat      = `de'i li 2006 pi'e 01 pi'e 02 ti'u 15 pi'e 04 pi'e 05`
+	iOS13DetriFormat      = `Y2006 M01 2, Mon`
+	iOS13DetriTemciFormat = `Y2006 M01 2, Mon 15:04`
 )
 
 // Detri formats a datestamp for Lojban.
@@ -15,4 +17,14 @@ func Detri(t time.Time) string {
 // DetriTemci formats a timestamp for Lojban.
 func DetriTemci(t time.Time) string {
 	return t.Format(detriTemciFormat)
+}
+
+// IOS13Detri formats a datestamp like iOS 13 does with the Lojban locale.
+func IOS13Detri(t time.Time) string {
+	return t.Format(iOS13DetriFormat)
+}
+
+// IOS13DetriTemci formats a date/timestamp like iOS 13 does with the Lojban locale.
+func IOS13DetriTemci(t time.Time) string {
+	return t.Format(iOS13DetriTemciFormat)
 }
