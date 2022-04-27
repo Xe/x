e0a769832dc185e71efd9a37e67abb55b3f1488c
Xe <me@christine.website>
2022-04-27 03:01:34 +0000

    portable service for cadeybot
    Signed-off-by: Xe <me@christine.website>

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     gomod2nix.url = "github:tweag/gomod2nix";
+    portable-svc.url = "git+https://tulpa.dev/cadey/portable-svc.git?ref=main";
   };
 
-  outputs = { self, nixpkgs, utils, gomod2nix }:
+  outputs = { self, nixpkgs, utils, gomod2nix, portable-svc }:
     utils.lib.eachSystem [
       "x86_64-linux"
       "aarch64-linux"
@@ -23,6 +24,7 @@
               buildGoModule = prev.buildGo118Module;
             })
             gomod2nix.overlay
+            portable-svc.overlay
           ];
         };
 
@@ -33,28 +35,56 @@
           modules = ./gomod2nix.toml;
         };
 
-        copyFile = { pname, path ? pname }: pkgs.stdenv.mkDerivation {
-          inherit pname;
-          inherit (everything) version;
-          src = everything;
+        copyFile = { pname, path ? pname }:
+          pkgs.stdenv.mkDerivation {
+            inherit pname;
+            inherit (everything) version;
+            src = everything;
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src/bin/$pname $out/bin/$path
-          '';
-        };
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $src/bin/$pname $out/bin/$path
+            '';
+          };
       in {
         packages = rec {
           default = everything;
-          license = copyFile { pname = "license"; path = "xlicense"; };
-          makeMastodonApp = copyFile { pname = "mkapp"; path = "make-mastodon-app"; };
+          license = copyFile {
+            pname = "license";
+            path = "xlicense";
+          };
+          makeMastodonApp = copyFile {
+            pname = "mkapp";
+            path = "make-mastodon-app";
+          };
           prefix = copyFile { pname = "prefix"; };
           quickserv = copyFile { pname = "quickserv"; };
           within-website = copyFile { pname = "within.website"; };
           johaus = copyFile { pname = "johaus"; };
           cadeybot = copyFile { pname = "cadeybot"; };
           mainsanow = copyFile { pname = "mainsanow"; };
-          importer = copyFile { pname = "importer"; path = "cadeybot-importer"; };
+          importer = copyFile {
+            pname = "importer";
+            path = "cadeybot-importer";
+          };
+
+          robocadey = copyFile { pname = "robocadey"; };
+          robocadey-psvc = let
+            service = pkgs.substituteAll {
+              name = "robocadey.service";
+              src = ./run/robocadey.service.in;
+              robocadey = self.packages.${system}.robocadey;
+            };
+          in pkgs.portableService {
+            inherit (self.packages.${system}.robocadey) version;
+            name = "printerfacts";
+            description = "Printer facts";
+            units = [ service ];
+            symlinks = [{
+              object = "${pkgs.cacert}/etc/ssl";
+              symlink = "/etc/ssl";
+            }];
+          };
         };
 
         devShells.default = pkgs.mkShell {
