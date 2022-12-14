f01c4273bd0dae6cacdcbc15cf7cdb0dd4f8b93d
Xe <me@christine.website>
2022-12-13 20:31:59 -0500

    nix-community/gomod2nix
    Signed-off-by: Xe <me@christine.website>

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     };
 
     gomod2nix = {
-      url = "github:tweag/gomod2nix";
+      url = "github:nix-community/gomod2nix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.utils.follows = "utils";
     };
