4ab6f87505e29d112d0390a8af9a4540f73c8f28
Xe <me@christine.website>
2023-01-01 23:11:00 -0500

    cmd/hlang: test
    Signed-off-by: Xe <me@christine.website>

--- cmd/hlang/h/parser_test.go
+++ cmd/hlang/h/parser_test.go
@@ -2,6 +2,12 @@ package h
 
 import "testing"
 
+func TestParse(t *testing.T) {
+	if _, err := Parse("h h h"); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func BenchmarkParse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		if _, err := Parse("h h h"); err != nil {
--- cmd/hlang/run/run_test.go
+++ cmd/hlang/run/run_test.go
@@ -8,6 +8,12 @@ import (
 //go:embed testdata/h.wasm
 var bin []byte
 
+func TestRun(t *testing.T) {
+	if _, err := Run(bin); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func BenchmarkRun(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		if _, err := Run(bin); err != nil {
