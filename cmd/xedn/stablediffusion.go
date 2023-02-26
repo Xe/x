55262ff07c3d9e34d58a1760e6fd0ea96ec00904
Xe Iaso <me@xeiaso.net>
2023-02-26 09:24:10 -0500

    gomod2nix update
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ cmd/xedn/stablediffusion.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+
+	"within.website/x/internal/stablediffusion"
+)
+
+func diffuse(ctx context.Context, details stablediffusion.SimpleImageRequest) (path string, err error) {
+
+}
