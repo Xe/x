d6cd86cbc965b46880b4a6d5a25bcda19345f217
Xe Iaso <me@xeiaso.net>
2023-11-08 07:59:12 -0500

    bump bumperate.sh
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- bumperate.sh
+++ bumperate.sh
@@ -7,4 +7,4 @@ go mod vendor -o $OUT
 go run tailscale.com/cmd/nardump --sri $OUT > .go.mod.sri
 rm -rf $OUT
 
-perl -pi -e "s,# nix-direnv cache busting line:.*,# nix-direnv cache busting line: $(cat go.mod.sri)," flake.nix
+perl -pi -e "s,# nix-direnv cache busting line:.*,# nix-direnv cache busting line: $(cat .go.mod.sri)," flake.nix
