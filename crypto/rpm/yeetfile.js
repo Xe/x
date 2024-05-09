80cae31548245237044a82fd32731b679321eaf5
Xe Iaso <me@xeiaso.net>
2024-05-09 13:21:28 -0400

    add RPM cryotpgraphy
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ crypto/rpm/yeetfile.js
@@ -0,0 +1,12 @@
+rpm.build({
+    name: "xeiaso.net-keys",
+    description: "Public keys for xeiaso.net RPM packages",
+    homepage: "https://xeiaso.net",
+    license: "CC0",
+    goarch: "all",
+
+    build: (out) => {
+        yeet.run(`mkdir`, `-p`, `${out}/etc/pki/rpm-gpg`);
+        yeet.run(`cp`, `xeiaso.net-keys.asc`, `${out}/etc/pki/rpm-gpg/xeiaso.net-keys`);
+    },
+});
