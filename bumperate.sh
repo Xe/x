54bb74b2dc75af9ec1e34a396382b8558c1ef0a5
Xe Iaso <me@xeiaso.net>
2024-05-23 11:41:47 -0400

    bumperate: don't modify flake.nix, only modify .go.mod.sri and make direnv watch it
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- bumperate.sh
+++ bumperate.sh
@@ -6,5 +6,3 @@ rm -rf $OUT
 go mod vendor -o $OUT
 go run tailscale.com/cmd/nardump --sri $OUT > .go.mod.sri
 rm -rf $OUT
-
-perl -pi -e "s,# nix-direnv cache busting line:.*,# nix-direnv cache busting line: $(cat .go.mod.sri)," flake.nix
--- flake.nix
+++ flake.nix
@@ -1,5 +1,3 @@
-# nix-direnv cache busting line: sha256-EsfFTlyPnFw3627pAiAb6jOBIzC6YcL+1ql+Z6+99e0=
-
 {
   description = "/x/perimental code";
 
