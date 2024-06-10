e53338d968a924a8fda803c4a74f11177c2b12bd
Xe Iaso <me@xeiaso.net>
2024-06-09 23:16:47 -0400

    cmd/mi: use kustomize
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- cmd/mi/yeetfile.js
+++ cmd/mi/yeetfile.js
@@ -1,5 +1,5 @@
 nix.build(".#docker.mi");
 docker.load("./result");
 docker.push(`ghcr.io/xe/x/mi`);
-yeet.run("kubectl", "apply", "-f=manifest.yaml");
+yeet.run("kubectl", "apply", "-k=manifest");
 yeet.run("sh", "-c", "kubectl rollout restart -n mi deployments/mi");
