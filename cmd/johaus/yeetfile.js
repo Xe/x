d8367478b94af4492f41bee1c071a9a53e1e9700
Xe Iaso <me@xeiaso.net>
2024-09-02 09:33:55 -0400

    kube/alrest: move manifests here
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- cmd/johaus/yeetfile.js
+++ cmd/johaus/yeetfile.js
@@ -1,6 +1,3 @@
-nix.build(".#docker.johaus");
-docker.load("./result");
-docker.push(`ghcr.io/xe/x/johaus`);
 yeet.run("kubectl", "apply", "-f=manifest.yaml");
 yeet.run("sh", "-c", "kubectl rollout restart deployments/johaus");
 
