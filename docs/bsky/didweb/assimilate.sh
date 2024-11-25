cdba95496fd16baca8b6bcd7bb03302b73813dc1
Xe Iaso <me@xeiaso.net>
2024-11-25 14:35:35 -0500

    docs/bsky: add didweb notes
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ docs/bsky/didweb/assimilate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Docker
+apt-get update
+apt-get -y install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+adduser civo docker
+
+# rclone
+curl https://rclone.org/install.sh | sudo bash
---
+++ docs/bsky/didweb/pds.tf
@@ -0,0 +1,69 @@
+data "aws_route53_zone" "within_website" {
+  name = "within.website."
+}
+
+data "civo_ssh_key" "shiroko" {
+  name = "shiroko"
+}
+
+data "civo_disk_image" "ubuntu" {
+  filter {
+    key    = "name"
+    values = ["ubuntu-noble"]
+  }
+}
+
+resource "civo_network" "pds" {
+  label = "bsky-pds"
+}
+
+resource "civo_firewall" "pds" {
+  name                 = "bsky-pds"
+  network_id           = civo_network.pds.id
+  create_default_rules = false
+
+  ingress_rule {
+    label      = "yolo"
+    protocol   = "tcp"
+    port_range = "1-65535"
+    cidr       = ["0.0.0.0/0"]
+    action     = "allow"
+  }
+
+  egress_rule {
+    label      = "yolo"
+    protocol   = "tcp"
+    port_range = "1-65535"
+    cidr       = ["0.0.0.0/0"]
+    action     = "allow"
+  }
+}
+
+resource "civo_instance" "engram" {
+  hostname    = "engram"
+  tags        = ["xe", "pds"]
+  notes       = "Bluesky PDS for pds.within.website"
+  sshkey_id   = data.civo_ssh_key.shiroko.id
+  firewall_id = civo_firewall.pds.id
+  network_id  = civo_network.pds.id
+  size        = "g4s.xsmall"
+  disk_image  = data.civo_disk_image.ubuntu.diskimages[0].id
+  script      = file("${path.module}/assimilate.sh")
+  volume_type = "ms-xfs-2-replicas"
+}
+
+resource "aws_route53_record" "engram-within-website--A" {
+  zone_id = data.aws_route53_zone.within_website.zone_id
+  name    = "engram.${data.aws_route53_zone.within_website.name}"
+  type    = "A"
+  ttl     = "3600"
+  records = [civo_instance.engram.public_ip]
+}
+
+resource "aws_route53_record" "star-engram-within-website--A" {
+  zone_id = data.aws_route53_zone.within_website.zone_id
+  name    = "*.engram.${data.aws_route53_zone.within_website.name}"
+  type    = "A"
+  ttl     = "3600"
+  records = [civo_instance.engram.public_ip]
+}
