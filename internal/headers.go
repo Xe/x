e72ae5296075d3b5a51a2877c2f3f9aab588be03
Xe Iaso <me@xeiaso.net>
2025-01-19 12:23:12 -0500

    internal: disable immortal cache in development
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- internal/headers.go
+++ internal/headers.go
@@ -1,8 +1,16 @@
 package internal
 
-import "net/http"
+import (
+	"net/http"
+
+	"within.website/x"
+)
 
 func UnchangingCache(h http.Handler) http.Handler {
+	if x.Version == "devel" {
+		return h
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "public, max-age=31536000")
 		h.ServeHTTP(w, r)
