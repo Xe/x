0b25ec147dcfce6c0cb38443899c219c48e1dd92
Xe Iaso <me@xeiaso.net>
2025-04-02 11:14:06 -0400

    cmd/yeet/internal/mktarball: put VERSION in doc folder
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ cmd/quickserv/yeetfile.js
@@ -0,0 +1,17 @@
+["amd64", "arm64"].forEach(goarch => {
+    [deb, rpm, tarball].forEach(method => method.build({
+        name: "quickserv",
+        description: "Like python3 -m http.server but a single binary",
+        homepage: "https://within.website",
+        license: "CC0",
+        goarch,
+
+        documentation: {
+            "../../LICENSE": "LICENSE",
+        },
+
+        build: ({ bin }) => {
+            $`go build -o ${bin}/quickserv -ldflags '-s -w -extldflags "-static" -X "within.website/x.Version=${git.tag()}"'`
+        },
+    }))
+})
