5d25e91ccd642a7522468e5a4b5c434d84a65917
Xe Iaso <me@xeiaso.net>
2025-04-20 16:18:12 -0400

    homelab automount
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ pkgs/home-automount-nas/yeetfile.js
@@ -0,0 +1,12 @@
+rpm.build({
+    name: "home-automount-nas",
+    description: "Automount the NAS drive on boot",
+    homepage: "https://xeiaso.net",
+    license: "CC0",
+    goarch: "all",
+
+    build: ({ systemd }) => {
+        file.install("./mnt-itsuki.automount", `${systemd}/mnt-itsuki.automount`);
+        file.install("./mnt-itsuki.mount", `${systemd}/mnt-itsuki.mount`);
+    },
+});
